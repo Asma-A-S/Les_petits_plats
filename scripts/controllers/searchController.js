c9079711d822412fb7a0825c7840b7efdcc043f1
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-08-17 14:43:29 +0200

    class pour gérer les recherches par input ou par tag

---
+++ scripts/controllers/searchController.js
@@ -0,0 +1,43 @@
+import {
+	FilterByName,
+	FilterByAppliance,
+	FilterByIngredient,
+	FilterByUstensil,
+} from "../models/searchTemplate.js";
+
+export class SearchController {
+	constructor(recipes) {
+		this.originalRecipes = recipes;
+		this.filteredRecipes = [...this.originalRecipes];
+	}
+
+	// methode pour la recherche avancée
+	search(query, type = "name") {
+		switch (type) {
+			case "name":
+				this.filteredRecipes = new FilterByName(this.filteredRecipes).search(
+					query
+				);
+				break;
+			case "ingredient":
+				this.filteredRecipes = new FilterByIngredient(
+					this.filteredRecipes
+				).search(query);
+				break;
+			case "appliance":
+				this.filteredRecipes = new FilterByAppliance(
+					this.filteredRecipes
+				).search(query);
+				break;
+			case "ustensil":
+				this.filteredRecipes = new FilterByUstensil(
+					this.filteredRecipes
+				).search(query);
+				break;
+			default:
+				console.warn(`Type de recherche inconnu : ${type}`);
+				return this.filteredRecipes;
+		}
+		return this.filteredRecipes;
+	}
+}
