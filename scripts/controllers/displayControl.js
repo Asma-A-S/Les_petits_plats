a5776a1bf1269b80820b7cfd8628955862412f64
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-08-17 14:42:17 +0200

    fonctions pour gérer l'affichage générales des recettes et les liste déroulantes

---
+++ scripts/controllers/displayControl.js
@@ -0,0 +1,58 @@
+import { DropdownData } from "../models/dropdownModel.js";
+import { createRecipeCardDom } from "../models/recipeTemplate.js";
+
+// afficher les recettes
+export function displayRecipes(recipes) {
+	const container = document.querySelector(".container-card");
+	container.innerHTML = "";
+
+	recipes.forEach((recipe) => {
+		const card = createRecipeCardDom(recipe);
+		container.appendChild(card);
+	});
+}
+
+//mettre à jour le nombre de recettes affichées
+export function updateRecipeCount(count) {
+	const recipeCountElement = document.getElementById("recipeCount");
+	recipeCountElement.textContent = `${count} recettes`;
+}
+
+//rcéupérer et afficher les  listes ingredients ustensil et appliance
+export function getIngredientUstensilApplianceLists(dropdownData) {
+	displayDropdownLists(
+		"ingredient-dropdown",
+		dropdownData.ingredients,
+		"ingredient"
+	);
+	displayDropdownLists(
+		"appliance-dropdown",
+		dropdownData.appliances,
+		"appliance"
+	);
+	displayDropdownLists("ustensil-dropdown", dropdownData.ustensils, "ustensil");
+}
+
+// Mise à jour des listes déroulantes
+export function updateFiltredDropdowns(recipes) {
+	const dropdownData = new DropdownData(); //instance
+	dropdownData.setTagsLists(recipes);
+	const updatedDropdownLists = dropdownData.returnedDropdownLists();
+	getIngredientUstensilApplianceLists(updatedDropdownLists);
+}
+
+//squelette pour afficher les listes déroulantes
+export function displayDropdownLists(dropdownId, items, type) {
+	const dropdown = document.getElementById(dropdownId);
+	if (dropdown) {
+		dropdown.innerHTML = ""; // Efface les anciens éléments de la liste déroulante
+
+		items.forEach((item) => {
+			const li = document.createElement("li");
+			li.classList.add("dropdown-item", "tag-input");
+			li.textContent = item;
+			li.setAttribute("data-type", type);
+			dropdown.appendChild(li);
+		});
+	}
+}
